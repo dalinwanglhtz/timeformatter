c5fca629b976c81853de8752a8e067964c5313ba
Dale Wang <dalinwanglhtz@gmail.com>
2020-02-10 21:44:02 +1100

    refined working cut

--- src/main/java/com/codewardev/TimeFormatter.java
+++ src/main/java/com/codewardev/TimeFormatter.java
@@ -3,35 +3,33 @@ package com.codewardev;
 // https://www.codewars.com/kata/52742f58faf5485cae000b9a/train/java
 
 public class TimeFormatter {
+	
+	private static final int MINUTES = 60;
+	private static final int HOURS = MINUTES*60;
+	private static final int DAYS = HOURS*24;
+	private static final int YEARS = DAYS*365;
 
 	public static String formatDuration(int seconds) {
 		int numSeconds = seconds;
 		StringBuilder output = new StringBuilder();
-		int minutes = 60;
-		int hours = minutes*60;
-		int days = hours*24;
-		int years = 365*days;
 		
 		if(seconds == 0) {
 			return "now";
 		}
 		
-		int remainder = numSeconds;
+		int remainder = numSeconds % YEARS;
 		String year="";
-		if(numSeconds / years > 0) {
-			year = (numSeconds / years == 1? "1 year":(numSeconds / years)+" years");
-			remainder = numSeconds % years;
+		if(numSeconds / YEARS > 0) {
+			year = (numSeconds / YEARS == 1? "1 year":(numSeconds / YEARS)+" years");
 			output.append(year);
-		} else {
-			remainder = numSeconds % years;
 		}
 		
 		
 		numSeconds = remainder;
+		remainder = numSeconds % DAYS;
 		String day="";
-		if(numSeconds / days > 0) {
-			day = (numSeconds / days == 1? "1 day":(numSeconds / days)+" days");
-			remainder = numSeconds % days;
+		if(numSeconds / DAYS > 0) {
+			day = (numSeconds / DAYS == 1? "1 day":(numSeconds / DAYS)+" days");
 			if(!year.isEmpty()) {
 				if(remainder > 0) {
 					output.append(", "+day);
@@ -41,16 +39,14 @@ public class TimeFormatter {
 			} else {
 				output.append(day);
 			}
-		} else {
-			remainder = numSeconds % days;
 		}
 		
 		
 		numSeconds = remainder;
+		remainder = numSeconds % HOURS;
 		String hour="";
-		if(numSeconds / hours > 0) {
-			hour = (numSeconds / hours == 1? "1 hour":(numSeconds / hours)+" hours");
-			remainder = numSeconds % hours;
+		if(numSeconds / HOURS > 0) {
+			hour = (numSeconds / HOURS == 1? "1 hour":(numSeconds / HOURS)+" hours");
 			if(!year.isEmpty() || !day.isEmpty()) {
 				if(remainder > 0) {
 					output.append(", "+hour);
@@ -60,15 +56,13 @@ public class TimeFormatter {
 			} else {
 				output.append(hour);
 			}
-		} else {
-			remainder = numSeconds % hours;
 		}
 		
 		numSeconds = remainder;
+		remainder = numSeconds % MINUTES;
 		String minute="";
-		if(numSeconds / minutes > 0) {
-			minute = (numSeconds / minutes == 1? "1 minute":(numSeconds / minutes)+" minutes");
-			remainder = numSeconds % minutes;
+		if(numSeconds / MINUTES > 0) {
+			minute = (numSeconds / MINUTES == 1? "1 minute":(numSeconds / MINUTES)+" minutes");
 			if(!year.isEmpty() || !day.isEmpty() || !hour.isEmpty()) {
 				if(remainder > 0) {
 					output.append(", "+minute);
@@ -78,8 +72,6 @@ public class TimeFormatter {
 			} else {
 				output.append(minute);
 			}
-		} else {
-			remainder = numSeconds % minutes;
 		}
 		
 		String second="";
